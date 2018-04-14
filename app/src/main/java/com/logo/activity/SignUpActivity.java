b20db2b43473b9e4891c4af1794cfeb0d646d4b7
Mandeep <mspanesar145@gmail.com>
2018-04-14 11:23:25 +0530

    initial commit

--- app/src/main/java/com/logo/activity/SignUpActivity.java
+++ app/src/main/java/com/logo/activity/SignUpActivity.java
@@ -102,7 +102,7 @@ public class SignUpActivity extends LogoActivity  {
         editTextMobile = (EditText) findViewById(R.id.et_mobile);
         editTextEmail = (EditText) findViewById(R.id.et_email);
         editTextCity = (EditText) findViewById(R.id.et_city);
-        editTextPassword = (EditText) findViewById(R.id.et_password);
+        //editTextPassword = (EditText) findViewById(R.id.et_password);
         imageViewBack = (ImageView) findViewById(R.id.iv_back);
         buttonSignUp = (Button) findViewById(R.id.bt_signup);
 
